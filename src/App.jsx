07d8131a0cd0cff2e7710d3f8039d38921f1c58a
Deea Emilia Duta <deeaemiliad@gmail.com>
2023-04-05 19:46:46 +0300

    Added Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { FaTimes } from 'react-icons/fa';
 
 function getTasks() {
     return [
@@ -16,16 +17,28 @@ function getTasks() {
 }
 
 function App() {
+    const [tasks, setTasks] = useState([]);
+
     useEffect(() => {
-        console.log('test');
+        const data = getTasks();
+        setTasks(data);
     }, []);
+    //Note: cand ave un array de dependinte, useEffect se apeleaza la fiecare re-render
+    // cand avem un array de dependinte gol se apeleaza doar o data
+    // pentru a controla cand se apeleaza useEffect putem scrie variable in array-ul de dependinte
+
+    const deleteTask = (id) => {
+        const updatedTasks = tasks.filter((task) => task.id !== id);
+        setTasks(updatedTasks);
+    };
 
     return (
         <div className="container">
             <header className="header">
                 <h1>ToDo App</h1>
-                <Button color="red" text="Add" />
+                <Button text="Add" />
             </header>
+            <Tasks tasks={tasks} onDelete={deleteTask} />
         </div>
     );
 }
@@ -35,6 +48,29 @@ function Button({ color, text }) {
         <button className="btn" style={{ backgroundColor: color }}>
             {text}
         </button>
+    );
+}
+
+function Tasks({ tasks, onDelete }) {
+    return (
+        <>
+            {tasks.map((task, index) => (
+                <Task key={index} task={task} onDelete={onDelete} />
+            ))}
+        </>
+    );
+}
+
+function Task({ task, onDelete }) {
+    return (
+        <div className="task">
+            <h3>
+                {task.text}
+                <FaTimes style={{ color: 'red', cursor: 'pointer' }}
+                    onClick={() => onDelete(task.id) } />
+            </h3>
+            <p>{task.day}</p>
+        </div>
     );
 }
 
