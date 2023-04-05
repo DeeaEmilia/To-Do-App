af84eef58063f1894848ee99355ab5baf339cf71
Deea Emilia Duta <deeaemiliad@gmail.com>
2023-04-05 18:47:28 +0300

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+function getTasks() {
+    return [
+        {
+            id: 1,
+            text: 'Dentist',
+            day: '5 aprilie, 12:30',
+        },
+        {
+            id: 2,
+            text: 'Sedinta',
+            day: '6 aprilie, 9:30',
+        },
+    ];
+}
 
 function App() {
-  return <div>
-      
-    </div>;
+    useEffect(() => {
+        console.log('test');
+    }, []);
+
+    return (
+        <div className="container">
+            <header className="header">
+                <h1>ToDo App</h1>
+                <Button color="red" text="Add" />
+            </header>
+        </div>
+    );
+}
+
+function Button({ color, text }) {
+    return (
+        <button className="btn" style={{ backgroundColor: color }}>
+            {text}
+        </button>
+    );
 }
 
 export default App;
